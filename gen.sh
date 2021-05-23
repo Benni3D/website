ce8fdea024a72b8eaf311e98246a444551416635
Andreas Stürz <andreas@stuerz.xyz>
2021-05-24 00:09:04 +0200

    Update

--- gen.sh
+++ gen.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
 
-escape() {
-   sed 's/\\/\\\\/g;s/\//\\\//g;s/\./\\./g;'"s/\n//g" "$1"
-}
-
+src=content
 dest=generated
 
-rm -rf "$dest"
-mkdir -p "$dest"
+rm -rf "${dest}"
+mkdir -p "${dest}"
 
-for f in $(ls -1 content | tr '\n' ' '); do
-   file="content/$f"
-   [ ! -d "$file" ] && continue
-   cp -r "$file" "$dest/$f"
+# Generate bcc.html
+if [ ! -f "${dest}/bcc.1.html" ]; then
+   [ -f .bcc.1 ] || wget https://raw.githubusercontent.com/Benni3D/bcc/master/bcc.1 -O .bcc.1
+   groff -mandoc -Thtml ".bcc.1" | sed \
+      -e '/<!DOCTYPE[^$]\+$/d' \
+      -e '/"http:[^$]\+$/d' \
+      -e '1,19d' \
+      -e 's/<h1 align="center">bcc/<h1 align="center">Brainlet C Compiler/' \
+      -e '/^<\/body>$/d' \
+      -e '/^<\/html>$/d' \
+      -e 's/<p/<p class="bcc-p"/g' \
+      -e 's/<pre/<pre class="bcc-pre"/g' \
+      -e 's/<h2/<h2 class="bcc-h2"/g' \
+      -e 's/<a/<a class="bcc-a"/g' | sed \
+      -e '3i<table width="100%"><tr><td width="80%">' \
+      -e '12i<\/td><td>' \
+      -e '12i<img width="100%" src="img/bcc.png"><br>' \
+      -e '12i<a align="center" class="link2" href="https://github.com/Benni3D/bcc">GitHub<\/a>' \
+      -e '12i<\/td><\/tr><\/table>' \
+      > "${src}/bcc.html"
+fi
+
+for f in $(ls -1 "${src}" | tr '\n' ' '); do
+   file="content/${f}"
+   [ ! -d "${file}" ] && continue
+   cp -r "${file}" "$dest/$f"
 done
 
-for f in content/*.html; do
-   #sed "s/__REPLACE__/$(escape "$f")/g" "template.html" > "$dest/$(basename "$f")"
-   sed -e "/__REPLACE__/r $f" -e '/__REPLACE__/d' template.html > "$dest/$(basename "$f")"
+for f in "${src}"/*.html; do
+   sed -e "/__REPLACE__/r ${f}" -e '/__REPLACE__/d' template.html > "$dest/$(basename "$f")"
 done
 
