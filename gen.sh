a422896ef6fdd95f891b8d1f9b7e4cc76afacbef
BenniTec <benni@stuerz.xyz>
2021-06-05 22:05:21 +0200

    Changed button cursor

--- gen.sh
+++ gen.sh
@@ -26,7 +26,7 @@ if [ ! -f "${dest}/bcc.1.html" ]; then
       -e '12i<img width="100%" src="img/bcc.png"><br>' \
       -e '12i<a align="center" class="bcc-link" href="https://github.com/Benni3D/bcc">GitHub<\/a>' \
       -e '12i<\/td><\/tr><\/table>' \
-      -e '$a<p class="unimp">Generated using groff</p>' \
+      -e "\$a<p class=\"unimp\">Generated using groff on $(date -u +"%F %T (UTC)")</p>" \
       > "${src}/bcc.html"
 fi
 
